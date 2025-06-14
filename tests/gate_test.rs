bef599889d1d4d6eb53398411e542d49e2b4f17a
Tom Zeilmann <tom-zeilmann@t-online.de>
2025-06-14 21:54:38 +0200

    revise test structure and test for gate file

--- tests/gate_test.rs
+++ tests/gate_test.rs
@@ -3,48 +3,63 @@ use digital_logic_simulator::gate::{Gate, GateType};
 #[test]
 fn test_and_gate() {
     let mut gate = Gate::new(GateType::And, 2);
+
     gate.inputs = vec![true, true];
-    assert_eq!(gate.evaluate(), true);
+    gate.evaluate();
+    assert_eq!(gate.output, true);
 
     gate.inputs = vec![true, false];
-    assert_eq!(gate.evaluate(), false);
+    gate.evaluate();
+    assert_eq!(gate.output, false);
 
     gate.inputs = vec![false, false];
-    assert_eq!(gate.evaluate(), false);
+    gate.evaluate();
+    assert_eq!(gate.output, false);
 }
 
 #[test]
 fn test_or_gate() {
     let mut gate = Gate::new(GateType::Or, 2);
+
     gate.inputs = vec![false, false];
-    assert_eq!(gate.evaluate(), false);
+    gate.evaluate();
+    assert_eq!(gate.output, false);
 
     gate.inputs = vec![true, false];
-    assert_eq!(gate.evaluate(), true);
+    gate.evaluate();
+    assert_eq!(gate.output, true);
 
     gate.inputs = vec![true, true];
-    assert_eq!(gate.evaluate(), true);
+    gate.evaluate();
+    assert_eq!(gate.output, true);
 }
 
 #[test]
 fn test_xor_gate() {
     let mut gate = Gate::new(GateType::Xor, 2);
+
     gate.inputs = vec![false, false];
-    assert_eq!(gate.evaluate(), false);
+    gate.evaluate();
+    assert_eq!(gate.output, false);
 
     gate.inputs = vec![true, false];
-    assert_eq!(gate.evaluate(), true);
+    gate.evaluate();
+    assert_eq!(gate.output, true);
 
     gate.inputs = vec![true, true];
-    assert_eq!(gate.evaluate(), false);
+    gate.evaluate();
+    assert_eq!(gate.output, false);
 }
 
 #[test]
 fn test_not_gate() {
     let mut gate = Gate::new(GateType::Not, 1);
+
     gate.inputs = vec![false];
-    assert_eq!(gate.evaluate(), true);
+    gate.evaluate();
+    assert_eq!(gate.output, true);
 
     gate.inputs = vec![true];
-    assert_eq!(gate.evaluate(), false);
+    gate.evaluate();
+    assert_eq!(gate.output, false);
 }
